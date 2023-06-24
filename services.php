26dd9259c51be155f56f90ce2f0c49339d1e7e65
Aintariksa <sofiaadam2307@gmail.com>
2023-06-24 22:07:09 +0700

    Quiz
    Sedang mencoba membuat QUIZ.

--- services.php
+++ services.php
@@ -1,100 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- mobile metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-      <!-- site metas -->
-      <title>Services</title>
-      <meta name="keywords" content="">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <!-- owl carousel style -->
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.2.4/assets/owl.carousel.min.css" />
-      <!-- bootstrap css -->
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-      <!-- style css -->
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <!-- Responsive-->
-      <link rel="stylesheet" href="css/responsive.css">
-      <!-- fevicon -->
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
-      <!-- Scrollbar Custom CSS -->
-      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-      <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-      <!-- owl stylesheets --> 
-      <link rel="stylesheet" href="css/owl.carousel.min.css">
-      <link rel="stylesheet" href="css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-   </head>
-   <body>
-      <!--header section start -->
-      <?php require_once 'navbar.php'; ?>
-      <!--header section end -->
-      <!--services section start -->
-      <div class="services_section layout_padding">
-         <div class="container">
-            <h1 class="service_taital"><span class="our_text">Our</span> Services</h1>
-            <p class="service_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered </p>
-            <div class="services_section_2">
-               <div class="row">
-                  <div class="col-sm-4">
-                     <div class="icon_1"><img src="images/icon-1.png"></div>
-                     <h4 class="design_text">Logo Design</h4>
-                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
-                     <div class="icon_1"><img src="images/icon-2.png"></div>
-                     <h4 class="design_text">Web Development</h4>
-                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="icon_3"><img src="images/icon-3.png"></div>
-                     <h4 class="design_text">Web Design</h4>
-                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
-                     <div class="read_bt_2"><a href="#">Read More</a></div>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="icon_1"><img src="images/icon-4.png"></div>
-                     <h4 class="design_text">Banner Design</h4>
-                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
-                     <div class="icon_1"><img src="images/icon-5.png"></div>
-                     <h4 class="design_text">Social Media Work</h4>
-                     <p class="lorem_text">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!--services section end -->
-      <!--footer section start -->
-      <?php require_once 'footer.php'; ?>
-      <!--footer section end -->
-      <!--copyright section start -->
-      <div class="copyright_section">
-         <div class="container">
-            <p class="copyright_text">Copyright 2023 All Right Reserved By <a href="https://html.design">Free Html Templates</a> Distributed by: <a href="https://themewagon.com">ThemeWagon</a></p>
-         </div>
-      </div>
-      <!--copyright section end -->
-      <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <script src="js/plugin.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-      <!-- javascript --> 
-      <script src="js/owl.carousel.js"></script>
-      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script> 
-      <script type="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2//2.0.0-beta.2.4/owl.carousel.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-      <script src="../../assets/js/vendor/popper.min.js"></script>
-      <script src="../../dist/js/bootstrap.min.js"></script>
-   </body>
-</html>
+<?php
+// Ambil data dari API Trivia
+$url = 'https://opentdb.com/api.php?amount=20&category=19&difficulty=easy&type=boolean';
+$response = file_get_contents($url);
+
+// Memeriksa apakah permintaan berhasil
+if ($response === false) {
+    // Error dalam permintaan
+    die('Error saat mengambil data dari API Trivia');
+}
+
+// Mendekode respons JSON
+$data = json_decode($response, true);
+
+// Memeriksa apakah ada error dalam respons
+if ($data['response_code'] !== 0) {
+    // Error dalam respons
+    die('Error dalam respons API Trivia: ' . $data['response_code']);
+}
+
+// Data trivia tersedia dalam $data['results']
+$results = $data['results'];
+
+// Inisialisasi skor awal
+$score = 0;
+
+// Menampilkan pertanyaan dan menerima jawaban dari pengguna
+foreach ($results as $index => $result) {
+    $question = $result['question'];
+    $correctAnswer = $result['correct_answer'];
+
+    // Menampilkan pertanyaan dan pilihan jawaban
+    echo 'Pertanyaan ' . ($index + 1) . ': ' . $question . '<br>';
+    echo 'Jawaban (True/False): <br>';
+    echo '<form method="post" action="quiz.php">';
+    echo '<input type="hidden" name="index" value="' . $index . '">';
+    echo '<input type="hidden" name="correctAnswer" value="' . $correctAnswer . '">';
+    echo '<input type="radio" name="answer" value="True"> True<br>';
+    echo '<input type="radio" name="answer" value="False"> False<br>';
+    echo '<input type="submit" value="Submit">';
+    echo '</form>';
+    echo '<br>';
+
+    // Memeriksa jawaban yang dikirim oleh pengguna
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $userAnswer = $_POST['answer'];
+
+        if ($userAnswer === $correctAnswer) {
+            // Jawaban benar
+            echo 'Jawaban Anda: ' . $userAnswer . ' (Benar)<br><br>';
+            $score++;
+        } else {
+            // Jawaban salah
+            echo 'Jawaban Anda: ' . $userAnswer . ' (Salah)<br>';
+            echo 'Jawaban yang benar: ' . $correctAnswer . '<br><br>';
+        }
+    }
+}
+
+// Menampilkan skor akhir
+echo 'Skor Akhir: ' . $score . '/' . count($results);
+?>
